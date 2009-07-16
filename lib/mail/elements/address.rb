5fa8d62f01ea57530cc668f878c2e6084c5e2d36
Mikel Lindsaar <raasdnil@gmail.com>
2009-07-16 22:36:29 +1000

    Got the parser parsing all values

--- lib/mail/elements/address.rb
+++ lib/mail/elements/address.rb
@@ -40,6 +40,8 @@ module Mail
         tree.local_dot_atom_text.text_value.strip
       elsif tree.respond_to?(:angle_addr)
         tree.angle_addr.addr_spec.local_part.text_value.strip
+      elsif tree.respond_to?(:addr_spec)
+        tree.addr_spec.local_part.text_value.strip
       else
         tree.local_part.text_value.strip
       end
