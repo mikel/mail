e3857b88d6c5cef1dc2fbbde27aca7f18f5ce399
Mikel Lindsaar <raasdnil@gmail.com>
2009-08-05 17:27:03 +1000

    Missed Resent-Sender field.  Now fixed

--- lib/mail/fields/resent_sender_field.rb
+++ lib/mail/fields/resent_sender_field.rb
@@ -3,5 +3,21 @@
 module Mail
   class ResentSenderField < StructuredField
     
+    include Mail::AddressField
+    
+    FIELD_NAME = 'resent-sender'
+
+    def initialize(*args)
+      super(FIELD_NAME, strip_field(FIELD_NAME, args.last))
+    end
+
+    def addresses
+      [address]
+    end
+
+    def address
+      result = tree.addresses.first
+    end
+    
   end
 end
