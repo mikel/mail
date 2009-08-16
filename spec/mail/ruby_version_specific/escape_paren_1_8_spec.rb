cc6b5122623ac00eb9b67c0a0c41b6a0fa2c8087
Mikel Lindsaar <raasdnil@gmail.com>
2009-08-16 10:34:54 +1000

    Fixed up ascii_only? String extension spec for Ruby 1.8 to add edge cases

--- spec/mail/ruby_version_specific/escape_paren_1_8_spec.rb
+++ spec/mail/ruby_version_specific/escape_paren_1_8_spec.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "Ruby 1.8 Extensions" do
   
-  describe "string" do
+  describe "string ascii detection" do
     it "should say it is US-ASCII only if it is" do
       "abc".should be_ascii_only
     end
@@ -15,5 +15,18 @@ describe "Ruby 1.8 Extensions" do
     it "should not say it is US-ASCII only if it is a mix" do
       "abcかきくけこ123".should_not be_ascii_only
     end
+    
+    it "should handle edge cases" do
+      ["\x00", "\x01", "\x40", "\x7f", "\x73"].each do |str|
+        str.should be_ascii_only
+      end
+
+      ["\x81", "\x99", "\xFF"].each do |str|
+        str.should_not be_ascii_only
+      end
+
+    end
   end
 end
+
+
