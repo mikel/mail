5bc59539e7f4856dc2f832104313a513159cf862
Jeremy Kemper <jeremy@bitsweat.net>
2009-11-22 14:53:37 -0800

    Rely on Active Support's blank? method. Rely on load path for requires rather than absolute filesystem paths.

--- lib/mail/core_extensions.rb
+++ lib/mail/core_extensions.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 module Mail
-  require File.join(File.dirname(__FILE__), 'core_extensions/blank')
-  require File.join(File.dirname(__FILE__), 'core_extensions/nil')
-  require File.join(File.dirname(__FILE__), 'core_extensions/string')
+  require 'active_support/core_ext/object/blank'
+  require 'mail/core_extensions/nil'
+  require 'mail/core_extensions/string'
 end
