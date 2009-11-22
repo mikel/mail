bad9113e92d53c48db9fbd6daa30b60247a97353
Jeremy Kemper <jeremy@bitsweat.net>
2009-11-22 15:16:48 -0800

    Accept core_ext/blank from AS <= 2.3.4

--- lib/mail/core_extensions.rb
+++ lib/mail/core_extensions.rb
@@ -1,6 +1,11 @@
 # encoding: utf-8
 module Mail
-  require 'active_support/core_ext/object/blank'
+  begin
+    require 'active_support/core_ext/object/blank'
+  rescue LoadError
+    # For Active Support <= 2.3.4
+    require 'active_support/core_ext/blank'
+  end
   require 'mail/core_extensions/nil'
   require 'mail/core_extensions/string'
 end
