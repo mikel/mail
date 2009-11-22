a3c9e56c83acd36fd77e3f6651d2b872665ebb83
Jeremy Kemper <jeremy@bitsweat.net>
2009-11-23 07:23:42 +0800

    Can omit for AS <= 3.0

--- lib/mail/core_extensions.rb
+++ lib/mail/core_extensions.rb
@@ -3,8 +3,7 @@ module Mail
   begin
     require 'active_support/core_ext/object/blank'
   rescue LoadError
-    # For Active Support <= 2.3.4
-    require 'active_support/core_ext/blank'
+    # Unneeded for Active Support <= 3.0.pre
   end
   require 'mail/core_extensions/nil'
   require 'mail/core_extensions/string'
