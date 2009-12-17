f0dc07f9ee4f37d7295d3bf6a6d33cf866084a77
Mikel Lindsaar <raasdnil@gmail.com>
2009-12-17 13:14:45 +1100

    Vendor'd treetop

--- lib/vendor/treetop.rb
+++ lib/vendor/treetop.rb
@@ -1,5 +1,5 @@
 # Have to vendor treetop to avoid loading polyglot
 
-$:.unshift "#{File.dirname(__FILE__)}/treetop-1.4.3"
-TREETOP_DISABLE_POLYGLOT = true
+$:.unshift "#{File.dirname(__FILE__)}/treetop-1.4.3/lib"
+::TREETOP_DISABLE_POLYGLOT = true
 require 'treetop'
