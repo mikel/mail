c8d442c3a8463b621cf23c05a0f0aa5077f693c9
Jeremy Kemper <jeremy@bitsweat.net>
2010-03-07 14:02:50 -0800

    Explicitly require treetop compiler for address list parser spec

--- spec/mail/parsers/address_lists_parser_spec.rb
+++ spec/mail/parsers/address_lists_parser_spec.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 require File.join(File.dirname(File.expand_path(__FILE__)), '..', '..', 'spec_helper')
 
+require 'treetop/compiler'
 Treetop.load(File.join(MAIL_ROOT, 'lib/mail/parsers/rfc2822_obsolete'))
 Treetop.load(File.join(MAIL_ROOT, 'lib/mail/parsers/rfc2822'))
 Treetop.load(File.join(MAIL_ROOT, 'lib/mail/parsers/address_lists'))
