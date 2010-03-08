fe44d5df8b171d934aaa2394a806816d4c6a0bb7
Jeremy Kemper <jeremy@bitsweat.net>
2010-03-08 13:53:19 -0800

    ~/tmp is not expanded as this code expects. Use spec/tmp dir.

--- spec/mail/network/delivery_methods/file_delivery_spec.rb
+++ spec/mail/network/delivery_methods/file_delivery_spec.rb
@@ -24,9 +24,11 @@ describe "SMTP Delivery Method" do
   end
   
   describe "general usage" do
+    tmpdir = File.expand_path('../../../../tmp/mail', __FILE__)
+
     it "should send an email to a file" do
       Mail.defaults do
-        delivery_method :file, :location => File.expand_path('../../../../tmp/mail', __FILE__)
+        delivery_method :file, :location => tmpdir
       end
       
       mail = Mail.deliver do
@@ -42,7 +44,7 @@ describe "SMTP Delivery Method" do
 
     it "should send multiple emails to multiple files" do
       Mail.defaults do
-        delivery_method :file, :location => File.expand_path('../../../../tmp/mail', __FILE__)
+        delivery_method :file, :location => tmpdir
       end
       
       mail = Mail.deliver do
@@ -60,7 +62,7 @@ describe "SMTP Delivery Method" do
 
     it "should only create files based on the addr_spec of the destination" do
       Mail.defaults do
-        delivery_method :file, :location => "~/tmp/mail"
+        delivery_method :file, :location => tmpdir
       end
       
       mail = Mail.deliver do
