1e665e44448ded500b7bf5b82119e9fe7f40bd58
Ubiratan Pires Alberton <u.alberton@gmail.com>
2010-05-01 04:25:17 +0800

    Added recursive parsing of attachments inside message/rfc822 parts.

--- spec/mail/attachments_list_spec.rb
+++ spec/mail/attachments_list_spec.rb
@@ -19,7 +19,7 @@ describe "Attachments" do
   before(:each) do
     @mail = Mail.new
   end
-  
+
   describe "from direct content" do
     it "should work" do
       file_data = File.read(filename = fixture('attachments', 'test.png'))
@@ -39,9 +39,9 @@ describe "Attachments" do
       @mail.attachments['test.png'] = File.read(fixture('attachments', 'test.png'))
       @mail.attachments[0].filename.should == 'test.png'
     end
-    
+
   end
-  
+
   describe "from a supplied Hash" do
     it "should work" do
       file_data = File.read(filename = fixture('attachments', 'test.png'))
@@ -49,7 +49,7 @@ describe "Attachments" do
       @mail.attachments[0].filename.should == 'test.png'
       check_decoded(@mail.attachments[0].decoded, file_data)
     end
-    
+
     it "should allow you to override the content_type" do
       file_data = File.read(filename = fixture('attachments', 'test.png'))
       @mail.attachments['test.png'] = { :content => file_data,
@@ -63,7 +63,7 @@ describe "Attachments" do
                                         :mime_type => "application/x-gzip" }
       @mail.attachments[0].mime_type.should == 'application/x-gzip'
     end
-    
+
     it "should allow you to override the mime_type" do
       file_data = File.read(filename = fixture('attachments', 'test.png'))
       @mail.attachments['invoice.jpg'] = { :data => "you smiling",
@@ -75,7 +75,7 @@ describe "Attachments" do
   end
 
   describe "decoding and encoding" do
-    
+
     it "should set it's content_transfer_encoding" do
       file_data = File.read(filename = fixture('attachments', 'test.png'))
       @mail.attachments['test.png'] = { :content => file_data }
@@ -133,7 +133,7 @@ describe "Attachments" do
     end
 
   end
-  
+
   describe "setting the content type correctly" do
     it "should set the content type to multipart/mixed if none given and you add an attachment" do
       mail = Mail.new
@@ -147,7 +147,7 @@ end
 
 describe "reading emails with attachments" do
   describe "test emails" do
-    
+
     it "should find the attachment using content location" do
       mail = Mail.read(fixture(File.join('emails', 'attachment_emails', 'attachment_content_location.eml')))
       mail.attachments.length.should == 1
@@ -162,12 +162,12 @@ describe "reading emails with attachments" do
       mail = Mail.read(fixture(File.join('emails', 'attachment_emails', 'attachment_content_disposition.eml')))
       mail.attachments[0].filename.should == 'hello.rb'
     end
-    
+
     it "should decode an attachment" do
       mail = Mail.read(fixture(File.join('emails', 'attachment_emails', 'attachment_pdf.eml')))
       mail.attachments[0].decoded.length.should == 1026
     end
-    
+
     it "should find an attachment that has an encoded name value" do
       mail = Mail.read(fixture(File.join('emails', 'attachment_emails', 'attachment_with_encoded_name.eml')))
       mail.attachments.length.should == 1
@@ -178,6 +178,14 @@ describe "reading emails with attachments" do
         expected = "01 Quien Te Dij\212at. Pitbull.mp3"
       end
       result.should == expected
+    end
+
+    it "should find attachments inside parts with content-type message/rfc822" do
+      mail = Mail.read(fixture(File.join("emails",
+                                         "attachment_emails",
+                                         "attachment_message_rfc822.eml")))
+      mail.attachments.length.should == 1
+      mail.attachments[0].decoded.length.should == 1026
     end
 
   end
