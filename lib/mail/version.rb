fdb1fbfe9a53b179b5e319c6e061bce0e7ccc62d
Mikel Lindsaar <raasdnil@gmail.com>
2010-06-12 13:38:31 -0700

    Removing requirement on YAML to use version, and moving VERSION to a file in lib

--- lib/mail/version.rb
+++ lib/mail/version.rb
@@ -3,13 +3,19 @@ module Mail
   module VERSION
     
     def self.version
-      YAML.load_file(File.join(File.dirname(__FILE__), '../../', 'VERSION.yml'))
+      version_hash = {}
+      File.read(File.join(File.dirname(__FILE__), '../', 'VERSION')).each_line do |line|
+        type, value = line.chomp.split(":")
+        next if type =~ /^\s+$/  || value =~ /^\s+$/
+        version_hash[type] = value
+      end
+      version_hash
     end
     
-    MAJOR = version[:major]
-    MINOR = version[:minor]
-    PATCH = version[:patch]
-    BUILD = version[:build]
+    MAJOR = version['major']
+    MINOR = version['minor']
+    PATCH = version['patch']
+    BUILD = version['build']
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end
