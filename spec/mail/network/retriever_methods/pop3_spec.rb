968f274e438c29263a9e4c7ce801e5b1ee7c0184
Donald Ball <donald.ball@gmail.com>
2010-10-22 15:49:05 -0400

    Spec :delete_after_find behavior with a block

--- spec/mail/network/retriever_methods/pop3_spec.rb
+++ spec/mail/network/retriever_methods/pop3_spec.rb
@@ -85,6 +85,10 @@ describe "POP3 Retriever" do
       Mail.find(:delete_after_find => true)
       MockPOP3.popmails.first(10).each { |message| message.should be_deleted }
       MockPOP3.popmails.last(10).each { |message| message.should_not be_deleted }
+
+      Mail.find(:delete_after_find => true) { |message| }
+      MockPOP3.popmails.first(10).each { |message| message.should be_deleted }
+      MockPOP3.popmails.last(10).each { |message| message.should_not be_deleted }
     end
 
     it "should handle the find_and_delete method" do
