27f5aa9ee1e0acb27e3e7cf65b08df882cffab52
Mikel Lindsaar <mikel@rubyx.com>
2010-12-23 21:10:16 +1100

    Removing warning on @name not being initialized

--- lib/mail/fields/common/common_field.rb
+++ lib/mail/fields/common/common_field.rb
@@ -1,41 +1,42 @@
 # encoding: utf-8
 module Mail
   module CommonField # :nodoc:
+
     def name=(value)
       @name = value
     end
-    
+
     def name
-      @name
+      @name ||= nil
     end
-    
+
     def value=(value)
       @length = nil
       @tree = nil
       @element = nil
       @value = value
     end
-    
+
     def value
       @value
     end
-    
+
     def to_s
       decoded
     end
-    
+
     def default
       decoded
     end
-    
+
     def field_length
       @length ||= "#{name}: #{encode(decoded)}".length
     end
-    
+
     def responsible_for?( val )
       name.to_s.downcase == val.to_s.downcase
     end
-    
+
     private
 
     def strip_field(field_name, value)
