57d591a1cfd24af7aaf635ade60f11f669e1da53
yalab <rudeboyjet@gmail.com>
2010-12-31 15:59:40 +0900

    Multiple charset mail is be able to handle when ruby 1.9.

--- lib/mail/fields/unstructured_field.rb
+++ lib/mail/fields/unstructured_field.rb
@@ -166,6 +166,7 @@ module Mail
     end
         
     def encode(value)
+      value.encode!(charset) if defined?(Encoding) && charset
       (value.not_ascii_only? ? [value].pack("M").gsub("=\n", '') : value).gsub("\r", "=0D").gsub("\n", "=0A")
     end
     
