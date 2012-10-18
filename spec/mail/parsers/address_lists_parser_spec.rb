c9c66aab3165baaaf195e4c9814fd91d72902b2c
Paul Wittmann <paul@railslove.com>
2012-10-18 11:52:46 +0200

    CC fields with semicolon are now parsed right [fixes #228]

--- spec/mail/parsers/address_lists_parser_spec.rb
+++ spec/mail/parsers/address_lists_parser_spec.rb
@@ -12,4 +12,9 @@ describe "AddressListsParser" do
     a = Mail::AddressListsParser.new
     a.parse(text).should_not be_nil
   end
+  it "should parse an address list separated by semicolons" do
+    text = 'Mikel Lindsaar <test@lindsaar.net>; Friends: test2@lindsaar.net; Ada <test3@lindsaar.net>;'
+    a = Mail::AddressListsParser.new
+    a.parse(text).should_not be_nil
+  end
 end
