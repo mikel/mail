ab2347e7751f9e45135d34e16120b56d4fc5ed22
Mikel Lindsaar <mikel@reinteractive.net>
2012-11-18 14:28:00 +1100

    Adding debug output on rspec load

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@ require 'rspec'
 require File.join(File.dirname(__FILE__), 'matchers', 'break_down_to')
 
 require 'mail'
+STDERR.puts("Running Specs for Mail Version #{Mail::VERSION::STRING}")
 
 RSpec.configure do |c|
   c.mock_with :rspec
