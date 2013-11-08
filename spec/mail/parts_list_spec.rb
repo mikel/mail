c99241e77db4a54e27dda17e504e5bb8e67a1d35
thk <thk@runtastic.com>
2013-11-08 12:54:34 +0100

    Fix for multipart emails without a set content_type.

--- spec/mail/parts_list_spec.rb
+++ spec/mail/parts_list_spec.rb
@@ -17,4 +17,21 @@ describe "PartsList" do
     p << 'text/html'
     p.sort!(order)
   end
+
+  it "should not fail if we do not have a content_type" do
+    p = Mail::PartsList.new
+    order = ['text/plain', 'text/html']
+
+    no_content_type_part = Mail::Part.new
+    plain_text_part      = Mail::Part.new
+    html_text_part       = Mail::Part.new
+
+    no_content_type_part.content_type = nil
+    html_text_part.content_type       = 'text/html'
+
+    p << no_content_type_part
+    p << plain_text_part
+    p << html_text_part
+    p.sort!(order).should eq [plain_text_part, html_text_part, no_content_type_part]
+  end
 end
