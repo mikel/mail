d804a8c60d89da9d242e5b846cd2ffdc754a2f11
grosser <michael@grosser.it>
2014-07-22 19:35:59 -0700

    fix simple trailing whitespace

--- lib/mail/encodings/quoted_printable.rb
+++ lib/mail/encodings/quoted_printable.rb
@@ -5,7 +5,7 @@ module Mail
   module Encodings
     class QuotedPrintable < SevenBit
       NAME='quoted-printable'
-   
+
       PRIORITY = 2
 
       def self.can_encode?(str)
@@ -15,7 +15,10 @@ module Mail
       # Decode the string from Quoted-Printable. Cope with hard line breaks
       # that were incorrectly encoded as hex instead of literal CRLF.
       def self.decode(str)
-        str.gsub(/(?:=0D=0A|=0D|=0A)\r\n/, "\r\n").unpack("M*").first.to_lf
+        str = str.dup
+        str.gsub!(/(?:=0D=0A|=0D|=0A)\r\n/, "\r\n")
+        str.gsub!(/=$/, "") # https://bugs.ruby-lang.org/issues/7175 trailing = is not standard compliant, but it's common in practice
+        str.unpack("M*").first.to_lf
       end
 
       def self.encode(str)
@@ -25,12 +28,12 @@ module Mail
       def self.cost(str)
         # These bytes probably do not need encoding
         c = str.count("\x9\xA\xD\x20-\x3C\x3E-\x7E")
-        # Everything else turns into =XX where XX is a 
+        # Everything else turns into =XX where XX is a
         # two digit hex number (taking 3 bytes)
         total = (str.bytesize - c)*3 + c
         total.to_f/str.bytesize
       end
-        
+
       private
 
       Encodings.register(NAME, self)
