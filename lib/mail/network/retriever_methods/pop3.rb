mikel/mail

commit 1ab7d7b5442e0b7a44838725dc623866f4f99fde

use sort_by instead of sort

lib/mail/network/retriever_methods/pop3.rb
@@ -61,7 +61,7 @@ module Mail
       start do |pop3|
         mails = pop3.mails
         pop3.reset # Clears all "deleted" marks. This prevents non-explicit/accidental deletions due to server settings.
-        mails.sort! { |m1, m2| m2.number <=> m1.number } if options[:what] == :last
+        mails.sort_by! { |m| -1 * m.number } if options[:what] == :last
         mails = mails.first(options[:count]) if options[:count].is_a? Integer
         
         if options[:what].to_sym == :last && options[:order].to_sym == :desc ||

lib/mail/fields/common/parameter_hash.rb
@@ -40,7 +40,7 @@ module Mail
     end
 
     def encoded
-      map.sort { |a,b| a.first.to_s <=> b.first.to_s }.map! do |key_name, value|
+      map.sort_by { |a| a.first.to_s }.map! do |key_name, value|
         unless value.ascii_only?
           value = Mail::Encodings.param_encode(value)
           key_name = "#{key_name}*"
@@ -50,7 +50,7 @@ module Mail
     end
 
     def decoded
-      map.sort { |a,b| a.first.to_s <=> b.first.to_s }.map! do |key_name, value|
+      map.sort_by { |a| a.first.to_s }.map! do |key_name, value|
         %Q{#{key_name}=#{quote_token(value)}}
       end.join("; ")
     end

commit dcb6a631b573593bbee6fe64d0d0eb60f27c6a12

Fix 1.8.7 regression in 1ab7d7b5442e0b7a44838725dc623866f4f99fde
Array#sort_by! was added in Ruby 1.9.2.

lib/mail/network/retriever_methods/pop3.rb
@@ -61,7 +61,7 @@ module Mail
       start do |pop3|
         mails = pop3.mails
         pop3.reset # Clears all "deleted" marks. This prevents non-explicit/accidental deletions due to server settings.
-        mails.sort_by! { |m| -1 * m.number } if options[:what] == :last
+        mails.sort! { |m1, m2| m2.number <=> m1.number } if options[:what] == :last
         mails = mails.first(options[:count]) if options[:count].is_a? Integer
         
         if options[:what].to_sym == :last && options[:order].to_sym == :desc ||