45e60ff18bcd02b3de1a1db3e009122da0046a76
Robin Roestenburg <robin@roestenburg.io>
2014-09-15 14:15:49 +0200

    Specs for parsing Content-Type and Content-Disposition headers with empty filenames

--- spec/mail/fields/content_disposition_field_spec.rb
+++ spec/mail/fields/content_disposition_field_spec.rb
@@ -79,4 +79,43 @@ describe Mail::ContentDispositionField do
     end
   end
 
+  describe "finding a filename" do
+
+    it "should locate a filename if there is a filename" do
+      string = %q{Content-Disposition: attachment; filename=mikel.jpg}
+      c = Mail::ContentDispositionField.new(string)
+      expect(c.filename).to eq 'mikel.jpg'
+    end
+
+    it "should locate a name if there is no filename" do
+      string = %q{Content-Disposition: attachment; name=mikel.jpg}
+      c = Mail::ContentDispositionField.new(string)
+      expect(c.filename).to eq 'mikel.jpg'
+    end
+
+    it "should return an empty string when filename or name is empty" do
+      string = %q{Content-Disposition: attachment; filename=""}
+      c = Mail::ContentDispositionField.new(string)
+      expect(c.filename).to eq ''
+
+      string = %q{Content-Disposition: attachment; name=""}
+      c = Mail::ContentDispositionField.new(string)
+      expect(c.filename).to eq ''
+    end
+
+    it "should locate an encoded name as a filename" do
+      string = %q{Content-Disposition: attachment; name*=iso-2022-jp'ja'01%20Quien%20Te%20Dij%91at.%20Pitbull.mp3}
+      c = Mail::ContentDispositionField.new(string)
+      if RUBY_VERSION >= '1.9'
+        expected = "01 Quien Te Dij\221at. Pitbull.mp3".force_encoding(Encoding::BINARY)
+        result = c.filename.force_encoding(Encoding::BINARY)
+      else
+        expected = "01 Quien Te Dij\221at. Pitbull.mp3"
+        result = c.filename
+      end
+      expect(expected).to eq result
+    end
+
+  end
+
 end
