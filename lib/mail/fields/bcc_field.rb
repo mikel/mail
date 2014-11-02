1576361fa7d71c409ac9f54b4780e41f1852f125
Bob Potter <bobby.potter@gmail.com>
2014-11-02 13:12:24 -0800

    Remove redundant parse in address fields

--- lib/mail/fields/bcc_field.rb
+++ lib/mail/fields/bcc_field.rb
@@ -39,7 +39,6 @@ module Mail
     def initialize(value = '', charset = 'utf-8')
       @charset = charset
       super(CAPITALIZED_FIELD, strip_field(FIELD_NAME, value), charset)
-      self.parse
       self
     end
     
