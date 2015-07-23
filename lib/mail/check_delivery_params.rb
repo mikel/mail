8c8c3b3b55e5c5e07564db56b03204804641cbc2
Toshio Maki <kirika.k2@gmail.com>
2015-07-23 22:10:39 +0900

    Fix smtp_envelope_from allow blank for error mail.

--- lib/mail/check_delivery_params.rb
+++ lib/mail/check_delivery_params.rb
@@ -1,10 +1,6 @@
 module Mail
   module CheckDeliveryParams
     def check_delivery_params(mail)
-      if mail.smtp_envelope_from.blank?
-        raise ArgumentError.new('An SMTP From address is required to send a message. Set the message smtp_envelope_from, return_path, sender, or from address.')
-      end
-
       if mail.smtp_envelope_to.blank?
         raise ArgumentError.new('An SMTP To address is required to send a message. Set the message smtp_envelope_to, to, cc, or bcc address.')
       end
