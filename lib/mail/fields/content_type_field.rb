c63d03acc41e6e342f3aa7eb4ebfb0156db5fa7e
Alex Otenko <alex.otenko@menlosecurity.com>
2016-11-23 17:03:21 +0000

    Simplify Content-Type parsing regexps
    Closes #1051

--- lib/mail/fields/content_type_field.rb
+++ lib/mail/fields/content_type_field.rb
@@ -145,9 +145,7 @@ module Mail
       # TODO: check if there are cases where whitespace is not a separator
       val = val.
         gsub(/\s*=\s*/,'='). # remove whitespaces around equal sign
-        tr(' ',';').
-        squeeze(';').
-        gsub(';', '; '). #use '; ' as a separator (or EOL)
+        gsub(/[; ]+/, '; '). #use '; ' as a separator (or EOL)
         gsub(/;\s*$/,'') #remove trailing to keep examples below
 
       if val =~ /(boundary=(\S*))/i
@@ -157,9 +155,6 @@ module Mail
       end
 
       case
-      when val.chomp =~ /^\s*([\w\-]+)\/([\w\-]+)\s*;;+(.*)$/i
-        # Handles 'text/plain;; format="flowed"' (double semi colon)
-        "#{$1}/#{$2}; #{$3}"
       when val.chomp =~ /^\s*([\w\-]+)\/([\w\-]+)\s*;\s?(ISO[\w\-]+)$/i
         # Microsoft helper:
         # Handles 'type/subtype;ISO-8559-1'
@@ -186,7 +181,7 @@ module Mail
       when val =~ /^\s*$/
         'text/plain'
       else
-        ''
+        val
       end
     end
 
