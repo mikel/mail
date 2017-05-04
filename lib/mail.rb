fa6274a3767d1c71fe717ec943f91061ba5e0302
Andrew Metcalf <andrew@stripe.com>
2017-05-04 10:35:30 -0700

    Only monkeypatch Net::SMTP#tlsconnect for Ruby < 1.9.3

--- lib/mail.rb
+++ lib/mail.rb
@@ -28,7 +28,7 @@ module Mail # :doc:
   require 'mail/version'
 
   require 'mail/core_extensions/string'
-  require 'mail/core_extensions/smtp' if RUBY_VERSION < '1.9.3'
+  require 'mail/core_extensions/smtp'
   require 'mail/indifferent_hash'
 
   # Only load our multibyte extensions if AS is not already loaded
