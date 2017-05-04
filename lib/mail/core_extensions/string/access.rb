b84a755df1ca19070deba7cd3c91edc4ed6e7930
Andrew Metcalf <andrew@stripe.com>
2017-05-04 10:22:54 -0700

    Remove `first` and `to` core_extensions to String

--- lib/mail/core_extensions/string/access.rb
+++ lib/mail/core_extensions/string/access.rb
@@ -7,48 +7,6 @@
 # itself does not depend on ActiveSupport to avoid versioning conflicts
 
 class String
-  unless '1.9'.respond_to?(:force_encoding)
-    # Returns the beginning of the string up to the +position+ treating the string as an array (where 0 is the first character).
-    #
-    # Examples:
-    #   "hello".to(0)  # => "h"
-    #   "hello".to(2)  # => "hel"
-    #   "hello".to(10) # => "hello"
-    def to(position)
-      mb_chars[0..position].to_s
-    end
-
-    # Returns the first character of the string or the first +limit+ characters.
-    #
-    # Examples:
-    #   "hello".first     # => "h"
-    #   "hello".first(2)  # => "he"
-    #   "hello".first(10) # => "hello"
-    def first(limit = 1)
-      if limit == 0
-        ''
-      elsif limit >= size
-        self
-      else
-        mb_chars[0...limit].to_s
-      end
-    end
-  else
-    def to(position)
-      self[0..position]
-    end
-
-    def first(limit = 1)
-      if limit == 0
-        ''
-      elsif limit >= size
-        self
-      else
-        to(limit - 1)
-      end
-    end
-  end
-
   if Module.method(:const_get).arity == 1
     # Tries to find a constant with the name specified in the argument string:
     #
