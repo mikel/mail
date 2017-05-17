mikel/mail

commit a3a46fe9e624a4d435c260364e3a6aef0cb6c0a8

Removing the read_only option from delete_all method in IMAP

lib/mail/network/retriever_methods/imap.rb
@@ -103,7 +103,6 @@ module Mail
       mailbox = Net::IMAP.encode_utf7(mailbox)
 
       start do |imap|
-        options[:read_only] ? imap.select(options[:mailbox]) : imap.examine(options[:mailbox])
         imap.uid_search(['ALL']).each do |message_id|
           imap.uid_store(message_id, "+FLAGS", [Net::IMAP::DELETED])
         end

commit 4d1319696f48d9a2a010f81cb18ef741b1d89dae

IMAP: fix delete_all regression, no mailbox selected
Introduced by a3a46fe9e624a4d435c260364e3a6aef0cb6c0a8

Fixes #909

lib/mail/network/retriever_methods/imap.rb
@@ -120,6 +120,7 @@ module Mail
       mailbox = Net::IMAP.encode_utf7(mailbox)
 
       start do |imap|
+        imap.examine(mailbox)
         imap.uid_search(['ALL']).each do |uid|
           imap.uid_store(uid, "+FLAGS", [Net::IMAP::DELETED])
         end