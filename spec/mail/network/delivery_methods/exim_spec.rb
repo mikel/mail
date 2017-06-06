6a01e706c16f0659fa89359064ae67f8664b7a07
Brian Gernhardt <bcg65@cornell.edu>
2017-06-06 15:40:05 -0400

    Set `delivery_method :exim` in before block
    There's no reason to set the delivery method to SMTP before every test
    only to have each test set it to Exim.

--- spec/mail/network/delivery_methods/exim_spec.rb
+++ spec/mail/network/delivery_methods/exim_spec.rb
@@ -4,24 +4,13 @@ require 'spec_helper'
 
 describe "exim delivery agent" do
   
-  before(:each) do
-    # Reset all defaults back to original state
+  before do
     Mail.defaults do
-      delivery_method :smtp, { :address              => "localhost",
-                               :port                 => 25,
-                               :domain               => 'localhost.localdomain',
-                               :user_name            => nil,
-                               :password             => nil,
-                               :authentication       => nil,
-                               :enable_starttls_auto => true  }
+      delivery_method :exim
     end
   end
 
   it "should send an email using exim" do
-    Mail.defaults do
-      delivery_method :exim
-    end
-    
     mail = Mail.new do
       from    'roger@test.lindsaar.net'
       to      'marcel@test.lindsaar.net, bob@test.lindsaar.net'
@@ -36,10 +25,6 @@ describe "exim delivery agent" do
   describe "return path" do
 
     it "should send an email with a return-path using exim" do
-      Mail.defaults do
-        delivery_method :exim
-      end
-
       mail = Mail.new do
         to "to@test.lindsaar.net"
         from "from@test.lindsaar.net"
@@ -56,10 +41,6 @@ describe "exim delivery agent" do
     end
 
     it "should use the sender address is no return path is specified" do
-      Mail.defaults do
-        delivery_method :exim
-      end
-
       mail = Mail.new do
         to "to@test.lindsaar.net"
         from "from@test.lindsaar.net"
@@ -75,10 +56,6 @@ describe "exim delivery agent" do
     end
 
     it "should use the from address is no return path or sender are specified" do
-      Mail.defaults do
-        delivery_method :exim
-      end
-
       mail = Mail.new do
         to "to@test.lindsaar.net"
         from "from@test.lindsaar.net"
@@ -93,10 +70,6 @@ describe "exim delivery agent" do
     end
 
     it "should escape the return path address" do
-      Mail.defaults do
-        delivery_method :exim
-      end
-
       mail = Mail.new do
         to 'to@test.lindsaar.net'
         from '"from+suffix test"@test.lindsaar.net'
@@ -111,10 +84,6 @@ describe "exim delivery agent" do
     end
 
     it "should quote the destinations to ensure leading -hyphen doesn't confuse exim" do
-      Mail.defaults do
-        delivery_method :exim
-      end
-
       mail = Mail.new do
         to '-hyphen@test.lindsaar.net'
         from 'from@test.lindsaar.net'
@@ -143,10 +112,6 @@ describe "exim delivery agent" do
   end
 
   it "should escape evil haxxor attemptes" do
-    Mail.defaults do
-      delivery_method :exim, :arguments => nil
-    end
-    
     mail = Mail.new do
       from    '"foo\";touch /tmp/PWNED;\""@blah.com'
       to      'marcel@test.lindsaar.net'
@@ -155,7 +120,7 @@ describe "exim delivery agent" do
 
     expect(Mail::Sendmail).to receive(:call).with(
       '/usr/sbin/exim',
-      " -f \"\\\"foo\\\\\\\"\\;touch /tmp/PWNED\\;\\\\\\\"\\\"@blah.com\" --",
+      "-i -t -f \"\\\"foo\\\\\\\"\\;touch /tmp/PWNED\\;\\\\\\\"\\\"@blah.com\" --",
       nil,
       mail.encoded)
 
@@ -163,10 +128,6 @@ describe "exim delivery agent" do
   end
 
   it "should not raise errors if no sender is defined" do
-    Mail.defaults do
-      delivery_method :exim
-    end
-
     mail = Mail.new do
       to "to@somemail.com"
       subject "Email with no sender"
@@ -181,10 +142,6 @@ describe "exim delivery agent" do
   end
 
   it "should raise an error if no recipient if defined" do
-    Mail.defaults do
-      delivery_method :exim
-    end
-
     mail = Mail.new do
       from "from@somemail.com"
       subject "Email with no recipient"
