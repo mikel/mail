mikel/mail

commit 1be3e5fee9dce06776313ceaf5146ceaa8906969

Suppress `warning: ambiguous first argument; put parentheses or a space even after `/' operator`

tasks/ragel.rake
@@ -4,23 +4,23 @@ load 'lib/mail/parsers.rb'
 rule 'lib/mail/parsers/rfc5234_abnf_core_rules.rl' => 'lib/mail/parsers/rfc3629_utf8.rl'
 
 # All RFC 5322 parsers depend on ABNF core rules
-rule /rfc5322_.+\.rl\z/ => 'lib/mail/parsers/rfc5234_abnf_core_rules.rl'
+rule %r|/rfc5322_.+\.rl\z/| => 'lib/mail/parsers/rfc5234_abnf_core_rules.rl'
 
 # RFC 5322 parser depends on its submodules
 file 'lib/mail/parsers/rfc5322.rl' => FileList['lib/mail/parsers/rfc5322_*.rl']
 
 # Ruby parsers depend on Ragel parser definitions
 # (remove -L to include line numbers for debugging)
-rule /_parser\.rb\z/ => '.rl' do |t|
+rule %r|/_parser\.rb\z/| => '.rl' do |t|
   sh "ragel -s -R -L -F1 -o #{t.name} #{t.source}"
 end
 
 # Dot files for Ragel parsers
-rule /_parser\.dot\z/ => '.rl' do |t|
+rule %r|/_parser\.dot\z/| => '.rl' do |t|
   sh "ragel -s -V -o #{t.name} #{t.source}"
 end
 
-rule /_parser\.svg\z/ => '.dot' do |t|
+rule %r|/_parser\.svg\z/| => '.dot' do |t|
   sh "dot -v -Tsvg -Goverlap=scale -o #{t.name} #{t.source}"
 end
 

commit 322ba78b0447c0cf0cbfe539757cb51cdca3979f

Fix regexps syntax broken in 1be3e5fee9dce06776313ceaf5146ceaa8906969

tasks/ragel.rake
@@ -4,23 +4,23 @@ load 'lib/mail/parsers.rb'
 rule 'lib/mail/parsers/rfc5234_abnf_core_rules.rl' => 'lib/mail/parsers/rfc3629_utf8.rl'
 
 # All RFC 5322 parsers depend on ABNF core rules
-rule %r|/rfc5322_.+\.rl\z/| => 'lib/mail/parsers/rfc5234_abnf_core_rules.rl'
+rule %r|rfc5322_.+\.rl\z| => 'lib/mail/parsers/rfc5234_abnf_core_rules.rl'
 
 # RFC 5322 parser depends on its submodules
 file 'lib/mail/parsers/rfc5322.rl' => FileList['lib/mail/parsers/rfc5322_*.rl']
 
 # Ruby parsers depend on Ragel parser definitions
 # (remove -L to include line numbers for debugging)
-rule %r|/_parser\.rb\z/| => '.rl' do |t|
+rule %r|_parser\.rb\z| => '.rl' do |t|
   sh "ragel -s -R -L -F1 -o #{t.name} #{t.source}"
 end
 
 # Dot files for Ragel parsers
-rule %r|/_parser\.dot\z/| => '.rl' do |t|
+rule %r|_parser\.dot\z| => '.rl' do |t|
   sh "ragel -s -V -o #{t.name} #{t.source}"
 end
 
-rule %r|/_parser\.svg\z/| => '.dot' do |t|
+rule %r|_parser\.svg\z| => '.dot' do |t|
   sh "dot -v -Tsvg -Goverlap=scale -o #{t.name} #{t.source}"
 end
 