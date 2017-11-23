81f7d83120e0366721d5d1996d8ae00c4c4d5a2a
Jakub Motycka <jakubonty@centrum.cz>
2017-11-23 12:47:14 +0100

    Fix transfer encoding when message encoding is blank
    Closes #1176

---
+++ spec/mail/encodings/transfer_encoding_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+describe Mail::Encodings::TransferEncoding do
+  it "accepts blank message_encoding" do
+    expect(described_class.negotiate('', '7bit', '')).to eq Mail::Encodings::SevenBit
+    expect(described_class.negotiate('', '8bit', '')).to eq Mail::Encodings::EightBit
+  end
+end
