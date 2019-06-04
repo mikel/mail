365c39a57fa87d389d45a6235f57dc28c214f290
pavel <pavel.rosicky@easy.cz>
2019-06-04 11:18:29 +0200

    update bundler

--- spec/travis-actionmailer.sh
+++ spec/travis-actionmailer.sh
@@ -4,6 +4,7 @@ set -v -e
 git clone --depth=1 https://github.com/rails/rails.git -b master
 cd rails
 echo -e "\ngem 'mail', path: '../'" >> Gemfile
-bundle update --source mail
+gem update bundler
+bundle update --bundler --source mail
 cd actionmailer
 bundle exec rake test
