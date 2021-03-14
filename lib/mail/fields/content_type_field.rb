76318c88af91ce17bfd9640ae73ae59dc076620f
Bianca Nenciu <nbianca@users.noreply.github.com>
2021-03-15 00:47:32 +0200

    Do not alter "name" field of Content-Type header (#1409)
    When Content-Type header parser fails, it tries to sanitize the value
    and then parse it again. One step of the sanitization process was to
    lowercase its value, which could change the meaning of "name" and
    "filename" fields as upper and lower case characters represent
    different things.

--- lib/mail/fields/content_type_field.rb
+++ lib/mail/fields/content_type_field.rb
@@ -123,8 +123,8 @@ module Mail
         gsub(/[; ]+/, '; '). #use '; ' as a separator (or EOL)
         gsub(/;\s*$/,'') #remove trailing to keep examples below
 
-      if val =~ /(boundary=(\S*))/i
-        val = "#{$`.downcase}boundary=#{$2}#{$'.downcase}"
+      if val =~ /((boundary|name|filename)=(\S*))/i
+        val = "#{$`.downcase}#{$2}=#{$3}#{$'.downcase}"
       else
         val.downcase!
       end
